5ab5d81903b21a97cc2c2b6bed2813964f5b3ddb
yurimalheiros <yurimalheiros@gmail.com>
2021-10-18 15:27:39 -0300

    Fix style

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,32 +2,32 @@ import React, { Component } from 'react';
 import Book from './Book';
 
 class BookShelf extends Component {
-    render() {
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books
-                            .filter((book) => book.shelf === this.props.shelf)
-                            .map((book) => (
-                                <li key={book.id}>
-                                    <Book 
-                                        id={book.id}
-                                        title={book.title}
-                                        authors={book.authors}
-                                        cover={book.cover}
-                                        shelf={book.shelf}
-                                        onUpdateShelf={this.props.onUpdateShelf}
-                                    />
-                                </li>
-                            )
-                        )}
-                    </ol>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books
+              .filter((book) => book.shelf === this.props.shelf)
+              .map((book) => (
+                <li key={book.id}>
+                  <Book
+                    id={book.id}
+                    title={book.title}
+                    authors={book.authors}
+                    cover={book.cover}
+                    shelf={book.shelf}
+                    onUpdateShelf={this.props.onUpdateShelf}
+                  />
+                </li>
+              )
+              )}
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default BookShelf;
--- src/Search.js
+++ src/Search.js
@@ -4,82 +4,82 @@ import * as BooksAPI from './BooksAPI'
 import Book from './Book';
 
 class Search extends Component {
-    state = {
-        query: '',
-        result: []
-    }
-    updateQuery = (event) => {
-        const inputData = event.target.value
-        this.setState({query: inputData})
+  state = {
+    query: '',
+    result: []
+  }
+  updateQuery = (event) => {
+    const inputData = event.target.value
+    this.setState({ query: inputData })
 
-        if (inputData) {
-            BooksAPI.search(inputData).then(res => {
-                if (('error' in res)) {
-                    this.setState(currentState => ({
-                        result: []
-                    }))
-                } else {
-                    const bookData = res.map((book) => ({
-                        id: book.id,
-                        title: book.title,
-                        authors: book.authors,
-                        cover: 'imageLinks' in book ? book.imageLinks.thumbnail : '',
-                        shelf: book.shelf
-                    }))
-                      
-                    this.setState(currentState => ({
-                        result: bookData
-                    }))
-                }
-            });
+    if (inputData) {
+      BooksAPI.search(inputData).then(res => {
+        if (('error' in res)) {
+          this.setState(currentState => ({
+            result: []
+          }))
         } else {
-            this.setState(currentState => ({
-                result: []
-            }))
+          const bookData = res.map((book) => ({
+            id: book.id,
+            title: book.title,
+            authors: book.authors,
+            cover: 'imageLinks' in book ? book.imageLinks.thumbnail : '',
+            shelf: book.shelf
+          }))
+
+          this.setState(currentState => ({
+            result: bookData
+          }))
         }
+      });
+    } else {
+      this.setState(currentState => ({
+        result: []
+      }))
     }
+  }
 
-    getBookShelf = (bookId) => {
-        const books = this.props.books.filter((book) => book.id === bookId)
-        return books.length > 0 ? books[0].shelf : 'none'
-    }
+  getBookShelf = (bookId) => {
+    const books = this.props.books.filter((book) => book.id === bookId)
+    return books.length > 0 ? books[0].shelf : 'none'
+  }
 
-    render() {
-        return (
-            <div className="search-books">
-                <div className="search-books-bar">
-                    <Link className="close-search" to="/">Close</Link>
-                    <div className="search-books-input-wrapper">
-                        <input
-                            type="text"
-                            value={this.state.query}
-                            onChange={this.updateQuery}
-                            placeholder="Search by title or author"
-                        />
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              value={this.state.query}
+              onChange={this.updateQuery}
+              placeholder="Search by title or author"
+            />
 
-                    </div>
-                </div>
-                <div className="search-books-results">
-                    <ol className="books-grid">
-                    {this.state.result
-                        .map((book) => (
-                            <li key={book.id}>
-                                <Book 
-                                    id={book.id}
-                                    title={book.title}
-                                    authors={book.authors}
-                                    cover={book.cover}
-                                    shelf={this.getBookShelf(book.id)}
-                                    onUpdateShelf={this.props.onUpdateShelf}
-                                />
-                            </li>
-                        )
-                    )}
-                    </ol>
-                </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.result
+              .map((book) => (
+                <li key={book.id}>
+                  <Book
+                    id={book.id}
+                    title={book.title}
+                    authors={book.authors}
+                    cover={book.cover}
+                    shelf={this.getBookShelf(book.id)}
+                    onUpdateShelf={this.props.onUpdateShelf}
+                  />
+                </li>
+              )
+              )}
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Search;
